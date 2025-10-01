6ed11a4f6c741479bf5f99038c9551359813fa20
makar182 <msavushkin@yandex.ru>
2025-10-01 22:19:41 +0300

    feat: show Telegram WebApp attributes

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+
+declare global {
+  interface TelegramWebApp {
+    [key: string]: unknown
+  }
+
+  interface TelegramNamespace {
+    WebApp?: TelegramWebApp
+  }
+
+  interface Window {
+    Telegram?: TelegramNamespace
+  }
+}
+
+export {}
