d33dee2d19d8d4f4054115393be0936d407c7aa9
makar182 <msavushkin@yandex.ru>
2025-10-06 23:59:22 +0300

    feat: integrate url shortener api

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,19 @@
 /// <reference types="vite/client" />
 
 declare global {
+  interface TelegramThemeParams {
+    bg_color?: string
+    button_color?: string
+    button_text_color?: string
+    secondary_bg_color?: string
+    hint_color?: string
+    text_color?: string
+    [key: string]: string | undefined
+  }
+
   interface TelegramWebApp {
+    themeParams?: TelegramThemeParams
+    ready?: () => void
     [key: string]: unknown
   }
 
